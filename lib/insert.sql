275a7c965bda2d2b330ec794cfd183bfcbe0ebea
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-12-31 18:30:04 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Mr.Chocolate", 15, "M", "Brown", "Adament", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Rowdy", 10, "M", "Black", "Jolly", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Tabitha", 7, "F", "Panda", "Bashful", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Sergeant Brown", 12, "M", "Brown", "Rash", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Melissa", 18, "F", "White", "Jolly", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Grinch", 5, "M", "Black", "Grumpy", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Wendy", 9, "F", "Grey", "Timid", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    NULL, 7, "M", "White", "Bold", True);
